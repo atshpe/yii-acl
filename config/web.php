8de3fe3d16710743319cc24d2ff93da0c09e75ee
yarchuk <zkhlnk.kiray2301@gmail.com>
2021-12-16 19:53:00 +0200

    Pretty URL

--- config/web.php
+++ config/web.php
@@ -43,14 +43,12 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
         // 'view' => [
         //     'class' => 'app\components\View',
         // ],
